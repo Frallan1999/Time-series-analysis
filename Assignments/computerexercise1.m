6927dd3fcfd24c84be4deb4a55281fc5faf4e002
Frallan1999 <98602349+Frallan1999@users.noreply.github.com>
2023-11-15 14:03:28 +0100

    first

--- Assignments/computerexercise1.m
+++ Assignments/computerexercise1.m
@@ -7,5 +7,14 @@ clear;
 close all;
 addpath('../functions', '../data')     % Add this line to update the path
 
-%  
-% hej amanda 
+%%  2.1 Working with time series in Matlab
+% create A and C polynomials for ARMA process 
+A1 = [1 -1.79 0.84];
+C1 = [1 -0.18 -0.11];
+A2 = [1 -1.79];
+C2 = [1 -0.18 -0.11];
+
+% Create ARMA polynomials 1 and 2 
+ARMA_poly_1 = idpoly(A1, [], C1);
+ARMA_poly_2 = idpoly(A2, [], C2);
+
