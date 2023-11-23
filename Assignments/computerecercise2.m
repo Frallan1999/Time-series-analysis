d548ae975edbfe2f8f8707934db93bd21afe81fc
Frallan1999 <98602349+Frallan1999@users.noreply.github.com>
2023-11-23 14:29:12 +0100

    h

---
+++ Assignments/computerecercise2.m
@@ -0,0 +1,11 @@
+%
+% Time series analysis
+% Computer exercise 2  
+%
+%
+clear; 
+close all;
+% addpath('functions', '/data')     % Add this line to update the path
+addpath('../functions', '../data')     % Add this line to update the path (Hanna)
+
+%% 
